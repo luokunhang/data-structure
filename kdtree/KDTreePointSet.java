d5104f87dce3e6ae2954f535f35d1226f22e2bc2
Brian Chan <brianc42@cs.washington.edu>
2019-10-16 23:41:20 -0700

    Add kdtree

---
+++ kdtree/KDTreePointSet.java
@@ -0,0 +1,28 @@
+package kdtree;
+
+import java.util.List;
+
+public class KDTreePointSet implements PointSet {
+    // TODO: add fields as necessary
+
+    /**
+     * Instantiates a new KDTree with the given points.
+     * @param points a non-null, non-empty list of points to include
+     *               (makes a defensive copy of points, so changes to the list
+     *               after construction don't affect the point set)
+     */
+    public KDTreePointSet(List<Point> points) {
+        // TODO: replace this with your code
+        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+    }
+
+    /**
+     * Returns the point in this set closest to (x, y) in (usually) O(log N) time,
+     * where N is the number of points in this set.
+     */
+    @Override
+    public Point nearest(double x, double y) {
+        // TODO: replace this with your code
+        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+    }
+}
