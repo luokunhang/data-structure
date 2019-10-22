bb6a71d42a5ee490ddcb977847816a7cdb549d66
quinn <kluo26@uw.edu>
2019-10-21 17:57:23 -0700

    first

--- heap/ArrayHeapMinPQ.java
+++ heap/ArrayHeapMinPQ.java
@@ -1,13 +1,18 @@
 package heap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
-    // TODO: add fields as necessary
+
+    private ArrayList<PriorityNode> items;
+    private HashMap<T, Integer> quick;
 
     public ArrayHeapMinPQ() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+        items = new ArrayList<>();
+        items.add(null);
+        quick = new HashMap<>();
     }
 
     /*
@@ -28,8 +33,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      * A helper method for swapping the items at two indices of the array heap.
      */
     private void swap(int a, int b) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+        quick.replace(items.get(a).getItem(), a, b);
+        quick.replace(items.get(b).getItem(), b, a);
+        PriorityNode temp = items.get(a);
+        items.set(a, items.get(b));
+        items.set(b, temp);
     }
 
     /**
@@ -40,8 +48,19 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      */
     @Override
     public void add(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+        if (contains(item)) {
+            throw new IllegalArgumentException("Already exists.");
+        }
+        items.add(new PriorityNode(item, priority));
+        quick.put(item, size());
+        swim(size());
+    }
+
+    private void swim(int n) {
+        if (n > 1 && items.get(n/2).getPriority() > items.get(n).getPriority()) {
+            swap(n, n/2);
+            swim(n/2);
+        }
     }
 
     /**
@@ -50,8 +69,7 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      */
     @Override
     public boolean contains(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet: replace this with your code.");
+        return quick.containsKey(item);
     }
 
     /**
@@ -61,8 +79,10 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      */
     @Override
     public T getSmallest() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet: replace this with your code.");
+        if (items.isEmpty()) {
+            throw new NoSuchElementException("PQ is empty.");
+        }
+        return items.get(1).getItem();
     }
 
     /**
@@ -72,8 +92,35 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      */
     @Override
     public T removeSmallest() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet: replace this with your code.");
+        if (items.isEmpty()) {
+            throw new NoSuchElementException("PQ is empty.");
+        }
+        quick.remove(items.get(1).getItem());
+        swap(1, size());
+        T keep = items.remove(size()).getItem();
+        sink(1);
+        return keep;
+    }
+
+    private void sink(int n) {
+        if (n * 2 + 1 <= size()) {
+            if (items.get(n).getPriority() > items.get(n * 2).getPriority() &&
+                    items.get(n).getPriority() > items.get(n * 2 + 1).getPriority()) {
+                if (items.get(n * 2 + 1).getPriority() < items.get(n * 2).getPriority()) {
+                    swap(n, n * 2 + 1);
+                } else {
+                    swap(n, n * 2);
+                }
+            } else if (items.get(n).getPriority() > items.get(n * 2).getPriority()) {
+                swap(n, n * 2);
+            } else if (items.get(n).getPriority() > items.get(n * 2 + 1).getPriority()) {
+                swap(n, n * 2 + 1);
+            }
+        } else if (n * 2 <= size()) {
+            if (items.get(n).getPriority() > items.get(n * 2).getPriority()) {
+                swap(n, n * 2);
+            }
+        }
     }
 
     /**
@@ -83,8 +130,14 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      */
     @Override
     public void changePriority(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet: replace this with your code.");
+        if (!contains(item)) {
+            throw new NoSuchElementException("Not an item in PQ.");
+        }
+        int currInd = quick.get(item);
+        items.get(currInd).priority = priority;
+        sink(currInd);
+        currInd = quick.get(item);
+        swim(currInd);
     }
 
     /**
@@ -93,7 +146,50 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      */
     @Override
     public int size() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet: replace this with your code.");
+        return items.size() - 1;
+    }
+
+    private class PriorityNode implements Comparable<ArrayHeapMinPQ.PriorityNode> {
+        private T item;
+        private double priority;
+
+        PriorityNode(T e, double p) {
+            this.item = e;
+            this.priority = p;
+        }
+
+        T getItem() {
+            return item;
+        }
+
+        double getPriority() {
+            return priority;
+        }
+
+        void setPriority(double priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(ArrayHeapMinPQ.PriorityNode other) {
+            if (other == null) {
+                return -1;
+            }
+            return Double.compare(this.getPriority(), other.getPriority());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || o.getClass() != this.getClass()) {
+                return false;
+            } else {
+                return ((ArrayHeapMinPQ.PriorityNode) o).getItem().equals(getItem());
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return item.hashCode();
+        }
     }
 }
--- heap/ArrayHeapMinPQTest.java
+++ heap/ArrayHeapMinPQTest.java
@@ -1,9 +1,46 @@
 package heap;
 
-import static org.junit.Assert.assertEquals;
+import deques.ArrayDeque;
+import deques.LinkedDeque;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
-    /* Be sure to write randomized tests that can handle millions of items. To
-     * test for runtime, compare the runtime of NaiveMinPQ vs ArrayHeapMinPQ on
-     * a large input of millions of items. */
+    @Test
+    public void testAddAndRemoveSmallest() {
+        // Random seed ensures that each test run is reproducible
+        int seed = 373; // or your favorite number
+        Random random = new Random(seed);
+
+        // Create a new ArrayDeque (reference implementation)
+        ExtrinsicMinPQ<Integer> expectedPQ = new NaiveMinPQ<>();
+        // Create a new LinkedDeque (testing implementation)
+        ExtrinsicMinPQ<Integer> testingPQ = new ArrayHeapMinPQ<>();
+
+        int ite = 5;
+        // Add 1000000 random integers to both the expectedDeque and testingDeque implementations
+        for (int i = 0; i < ite; i += 1) {
+            double p = random.nextDouble();
+            expectedPQ.add(i, p);
+            testingPQ.add(i, p);
+        }
+
+        for (int i = 0; i < ite; i += 1) {
+            double p = random.nextDouble();
+            expectedPQ.changePriority(i, p);
+            testingPQ.changePriority(i, p);
+        }
+
+        // Check that testingDeque matches expectedDeque on all 1000000 integers
+        for (int i = 0; i < ite; i += 1) {
+            int expectedValue = expectedPQ.removeSmallest();
+            int testingValue = testingPQ.removeSmallest();
+            assertEquals("Failed on iteration " + i, expectedValue, testingValue);
+            // To debug a particular iteration, set a conditional breakpoint
+            // Add a regular breakpoint but right click and set a condition like i == 57893
+        }
+    }
 }
