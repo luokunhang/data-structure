f2470cb444bfaeb0e758c804a92b89431202d9c3
quinn <kluo26@uw.edu>
2019-11-12 14:35:21 -0800

    first

--- astar/AStarSolver.java
+++ astar/AStarSolver.java
@@ -1,12 +1,27 @@
 package astar;
 
+import edu.princeton.cs.algs4.Stopwatch;
+import heap.ArrayHeapMinPQ;
+import heap.ExtrinsicMinPQ;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @see ShortestPathsSolver for more method documentation
  */
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
-    // TODO: add fields as necessary
+    private SolverOutcome outcome;
+    private double solutionWeight;
+    private List<Vertex> solution;
+    private double timeSpent;
+    private int numStatesExplored;
+
+    private Map<Vertex, Double> distTo;
+    private Map<Vertex, Vertex> edgeTo;
 
     /**
      * Immediately solves and stores the result of running memory optimized A*
@@ -14,38 +29,78 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
      * their results in constant time. The timeout is given in seconds.
      */
     public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+        Stopwatch sw = new Stopwatch();
+        numStatesExplored = 0;
+
+        ExtrinsicMinPQ<Vertex> fringe = new ArrayHeapMinPQ<>();
+        fringe.add(start, input.estimatedDistanceToGoal(start, end));
+
+        distTo = new HashMap<>();
+        edgeTo = new HashMap<>();
+        distTo.put(start, 0.0);
+        edgeTo.put(start, null);
+
+        while (!fringe.isEmpty()) {
+            Vertex current = fringe.removeSmallest();
+            numStatesExplored += 1;
+            if (current.equals(end)) {
+                solution = path(current);
+                solutionWeight = distTo.get(current);
+                outcome = SolverOutcome.SOLVED;
+                timeSpent = sw.elapsedTime();
+            }
+
+            List<WeightedEdge<Vertex>> children = input.neighbors(current);
+            for (WeightedEdge<Vertex> e: children) {
+                Vertex w = e.to();
+                //relax all edges
+                double pot = distTo.get(current) + e.weight();
+                if (!distTo.containsKey(w) || pot < distTo.get(w)) {
+                    distTo.put(w, distTo.get(current) + e.weight());
+                    edgeTo.put(w, current);
+                    if (!fringe.contains(w)) {
+                        fringe.add(w, distTo.get(w) + input.estimatedDistanceToGoal(w, end));
+                    }
+                }
+            }
+
+            if (sw.elapsedTime() > 300) {
+                outcome = SolverOutcome.UNSOLVABLE;
+                return;
+            }
+        }
+    }
+
+    private List<Vertex> path(Vertex end) {
+        Vertex curr = end;
+        Stack<Vertex> reverse = new Stack<>();
+        reverse.push(curr);
+        while (edgeTo.get(curr) != null) {
+            reverse.push(edgeTo.get(curr));
+            curr = edgeTo.get(curr);
+        }
+        List<Vertex> toReturn = new ArrayList<>();
+        while (!reverse.isEmpty()) {
+            toReturn.add(reverse.pop());
+        }
+        return toReturn;
     }
 
     @Override
-    public SolverOutcome outcome() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
-    }
+    public SolverOutcome outcome() { return outcome; }
 
     @Override
-    public List<Vertex> solution() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
-    }
+    public List<Vertex> solution() { return solution; }
 
     @Override
-    public double solutionWeight() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
-    }
+    public double solutionWeight() { return solutionWeight; }
 
     /** The total number of priority queue removeSmallest operations. */
     @Override
     public int numStatesExplored() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
+        return numStatesExplored;
     }
 
     @Override
-    public double explorationTime() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet; replace this with your code.");
-    }
+    public double explorationTime() { return timeSpent; }
 }
