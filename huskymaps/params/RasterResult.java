a1ade7b8fb34a779d21c3692abddd34f8802843e
Howard Xiao <thx@cs.washington.edu>
2019-11-14 20:57:14 -0800

    server: Give RasterResult serializable defaults

--- huskymaps/params/RasterResult.java
+++ huskymaps/params/RasterResult.java
@@ -10,19 +10,16 @@ public class RasterResult {
     /** The 2-dimensional string grid of map tilenames. */
     public final Rasterer.Tile[][] grid;
     /** The bounding upper-left, lower-right latitudes and longitudes of the final image. */
-    public final double ullat;
-    public final double ullon;
-    public final double lrlat;
-    public final double lrlon;
+    public final double ullat, ullon, lrlat, lrlon;
 
     /** Return a new RasterResult with the given grid. */
     public RasterResult(Rasterer.Tile[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             this.grid = null;
-            this.ullat = Double.NEGATIVE_INFINITY;
-            this.ullon = Double.NEGATIVE_INFINITY;
-            this.lrlat = Double.NEGATIVE_INFINITY;
-            this.lrlon = Double.NEGATIVE_INFINITY;
+            this.ullat = 0;
+            this.ullon = 0;
+            this.lrlat = 0;
+            this.lrlon = 0;
         } else {
             this.grid = grid;
             Rasterer.Tile gridUl = grid[0][0];
@@ -36,12 +33,8 @@ public class RasterResult {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         RasterResult that = (RasterResult) o;
         return Arrays.deepEquals(grid, that.grid);
     }
